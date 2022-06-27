4a13896e448ca2e45037e53171db711deab8039e
Deeplee <izuna@groovy.localdomain>
2022-06-27 20:26:19 +0700

    cfg & etc

---
+++ linux/dotfiles/autostart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nitrogen --restore &
+dwmblocks &
+setxkbmap -layout "us,ru" -option grp:ctrl_space_toggle
---
+++ linux/dotfiles/blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+        {"Battery:", "acpi | awk '{print $4}' | sed 's/,//g'", 5, 0},
+        {"SDD:", "df -h | grep /dev/nvme0n1p3 | awk '{print $3\"/\"$2}'", 30, 0},
+        {"HDD:", "df -h | grep /dev/sda | awk '{print $3\"/\"$2}'", 30, 0},
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"Swap:", "free -h | awk '/^Swap/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+};
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
+
