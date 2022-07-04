62a45f8e5d00ce3acf76f2073143b306d61d642d
David <dkapitsev@gmail.com>
2022-07-04 11:16:42 +0700

    reworked git.sh

---
+++ linux/sh/git.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: git.sh
+#
+#         USAGE: ./git.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: David Kapitsev(@1zuna)
+#  ORGANIZATION:
+#       CREATED: 06/30/2022 23:13
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+case $1 in
+	-a)
+		git add $2
+		;;
+	-c)
+		#read "" commit
+		git commit -m "$2"
+		;;
+	-r)
+		git rm -r --cached $2
+		;;
+	-p)
+		git push origin master
+		;;
+	*)
+		echo "Uknown option $1"
+		;;
+
+esac    # --- end of case ---
