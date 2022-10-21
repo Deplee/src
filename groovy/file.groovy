5129fad11e690fcd484d10a0923b675ce843c0d4
izuna <dkapitsev@gmail.com>
2022-10-21 12:23:12 +0700

    groovy

---
+++ groovy/file.groovy
@@ -0,0 +1,68 @@
+#!/usr/bin/env groovy
+/*def test = { c,g ->
+println c
+println g}
+test(5*2,6*3) */
+
+
+/*value = new File('C:\\Users\\19837606\\.vscode').eachFile {
+    it.name="/.*json/"
+    println it
+}*/
+
+/* dir path */
+def path='//mnt//hdd//groovy//file_operations'
+/*
+
+/* files count
+int files_counter = 0
+
+counter = new File(path)
+counter.eachFile {
+    files_counter += 1
+}
+
+println ("Count of files: $files_counter")
+*/
+/* get extensions */
+
+int extension_err = 0
+int f = 0
+def name = []
+def ex = new File(path).eachFile {
+    file_extension -> file_extension.getName().split("\\.")[-1]
+    def extension = file_extension.getName().split("\\.")[-1]
+        if (extension ==~ "png|rar|pdf|xlsx|xlx|doc|docx|pst") {
+                extension_err ++
+                //println (file_extension.getName())
+                //println (file_extension)
+                println (extension)
+        } else if (extension ==~ "txt"){
+            assert(extension ==~ null)
+    name.add(file_extension.getName())//.split("\\.")[0])
+    f++ }
+}
+    if (extension_err > 0) {
+    prntln "[Error]. File with extensions is exists. Extension is: $extension" }
+    for (int i=0 ; i < f ; i++){
+        println (name[i])
+    String main_name = name[i]
+    String result_name = '1_' + name[i] + '.bp'
+command = ["sh", "-c", "cp $main_name $result_name"]
+Runtime.getRuntime().exec((String[]) command.toArray())
+    }
+println "[LOG]. $f File(s) copied"
+println ("Count of err extensions: $extension_err")
+    if (extension_err > 0) {
+        prntln "[Error]. File with extensions is exists. Extension is: $extension"
+        exit(1)
+    }
+
+
+
+/* copy  windows
+
+source = '//mnt//hdd//groovy//file_operations//test.txt'
+target = '//mnt//hdd//groovy//file_operations//test.txt'
+new AntBuilder().copy( file:"$source", tofile:"$target") */
+
