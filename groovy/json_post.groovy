d2d79beff05d16dfdc3c637b6fe1aea9c0aa98f5
izuna <dkapitsev@gmail.com>
2022-10-21 16:08:07 +0700

    updated

--- groovy/json_post.groovy
+++ groovy/json_post.groovy
@@ -1,4 +1,4 @@
-def baseUrl = new URL('https://httpbin.org/post')
+/*def baseUrl = new URL('https://httpbin.org/post')
 def queryString = 'id=1&name=Test&job=sklad'
 def connection = baseUrl.openConnection()
 connection.with {
@@ -8,15 +8,37 @@ connection.with {
     writer << queryString
   }
   println content.text
+}*/
+/* GET method */
+try{
+  def get = new URL("https://httpbin.org/get").openConnection();
+  def getRC = get.getResponseCode();
+  println(getRC)
+  if (getRC.equals(200)){
+    post()
+  } else {
+    println "[ERROR]. HTTP GET Request status is not 200."
+  }
+} catch (Exception ex) {
+  println "[ERROR]. Exception from GET request."
 }
 
 
-def post = new URL ("https://httpbin.org/post").openConnection()
-def body = '{"name":"Test"}'
-post.setRequestMethod("POST")
-post.setRequestProperty("Content-Type","application/json")
-post.setDoOutput(true)
-post.getOutputStream().write(body.getBytes("UTF-8"))
-def postRC = post.getResponseCode()
-println(postRC)
-println(post.getInputStream().getText())
+ /* POST method */
+  static void post(){
+  try{
+    def post = new URL ("https://httpbin.org/post").openConnection()
+    def body = '{"name":"Test", "value": "5", "job": "tester", "phone": "1234567890"}'
+    post.setRequestMethod("POST")
+    post.setRequestProperty("Content-Type","application/json")
+    post.setDoOutput(true)
+    post.getOutputStream().write(body.getBytes("UTF-8"))
+    def postRC = post.getResponseCode()
+    //println(postRC)
+    println(post.getInputStream().getText())
+} catch(Exception ex){
+  println "[ERROR.] Exception from POST request."
+  }
+}
+
+/* post data */
