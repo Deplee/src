97146d8fd8373ae5e5a405faefc9fafa521e7c81
izuna <dkapitsev@gmail.com>
2022-10-21 13:37:31 +0700

    exceptions handler

--- groovy/file.groovy
+++ groovy/file.groovy
@@ -11,7 +11,7 @@ test(5*2,6*3) */
 }*/
 
 /* dir path */
-def path='//mnt//hdd//groovy//file_operations'
+def path='//mnt//hdd//src//groovy'
 import java.io.File
 /*
 
@@ -32,17 +32,17 @@ int f = 0
 def name = []
 try {
 def ex = new File(path).eachFile {
-    file_extension -> file_extension.getName().split("\\.")[-1]
-    def extension = file_extension.getName().split("\\.")[-1]
+    file_info -> file_info.getName().split("\\.")[-1]
+    def extension = file_info.getName().split("\\.")[-1]
         if (extension ==~ "png|rar|pdf|xlsx|xlx|doc|docx|pst") {
                 extension_err ++
                 //println (file_extension.getName())
                 //println (file_extension)
                 println (extension)
         } else if (extension ==~ "txt"){
-            assert(extension ==~ null)
-            name.add(file_extension.getName())//.split("\\.")[0])
-            f++ 
+            //assert(extension ==~ null)
+            name.add(file_info.getName())//.split("\\.")[0])
+            f++
     }
 }
     if (extension_err > 0){
@@ -65,8 +65,7 @@ if (extension_err > 0){
     prntln "[Error]. File with extensions is exists. Extension is: $extension"
     exit(1)
 }
-}
-catch (Exception ex){
+}catch (Exception ex){
     println "[ERROR]. Exception detected."
 }
 
@@ -75,4 +74,3 @@ catch (Exception ex){
 source = '//mnt//hdd//groovy//file_operations//test.txt'
 target = '//mnt//hdd//groovy//file_operations//test.txt'
 new AntBuilder().copy( file:"$source", tofile:"$target") */
-
